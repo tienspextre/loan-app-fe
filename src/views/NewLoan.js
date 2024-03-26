c72968c8af04e679c20c8cbb4b0c9624da7f3008
Nguyen Dang Tien <98021264+tienspextre@users.noreply.github.com>
2024-03-27 05:50:26 +0700

    fix newloan

--- src/views/NewLoan.js
+++ src/views/NewLoan.js
@@ -1,12 +1,15 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Header from '../components/Header';
-import UserHome from '../components/UserHome';
 import NewLoanForm from '../components/NewLoanForm';
+
 const NewLoan = () => {
   return (
     <div>
-        <Header/>
-        <NewLoanForm/>
+      <Header />
+      <div style={{ marginTop: '70px', overflowY: 'auto', maxHeight: 'calc(100vh - 70px)' }}>
+        {/* Adjust marginTop to match the height of your Header */}
+        <NewLoanForm />
+      </div>
     </div>
   );
 };
